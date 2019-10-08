e8f579df8e776247d98156c716628915bc2fbf96
Fred Saba <fred@fred-saba.com>
2019-10-07 20:07:41 -0700

    finalizing file structure

--- app/controller/htmlRoutes.js
+++ app/controller/htmlRoutes.js
@@ -4,7 +4,23 @@ let path = require("path")
 module.exports = function(app){
     
     app.get("/",function(req,res){
-    res.sendFile(path.join(__dirname+"/../view/index.html"))
+    res.sendFile(path.join(__dirname +"/../view/index.html"))
+
+    app.get("/reportfound",function(req,res){
+        res.sendFile(path.join(__dirname + "/../view/reportfound.html"))
+    })
+
+    app.get("/reportlost",function(req,res){
+        res.sendFile(path.join(__dirname + "/../view/reportlost.html"))
+    })
+
+    app.get("/viewfound",function(req,res){
+        res.sendFile(path.join(__dirname + "/../view/viewfound.html"))
+    })
+
+    app.get("/viewlost",function(req,res){
+        res.sendFile(path.join(__dirname + "/../view/viewlost.html"))
+    })
 })
 
 }
