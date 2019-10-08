16ba863493452afd55a52d0453b9dfd563e9a020
Jembae <jgilbertworks@gmail.com>
2019-10-08 14:31:45 -0700

    added upload image button, fixed for ids

--- app/javascript/reportLostLogic.js
+++ app/javascript/reportLostLogic.js
@@ -1,24 +1,22 @@
 let name = document.getElementById('name').value.trim();
-let email = document.getElementById('email').value.trim();
-let phone = parseInt(document.getElementById('phone').value.trim());
-let species = document.getElementById('species').value.trim();
-let breed = document.getElementById('breed').value.trim();
-let color = document.getElementById('color').value.trim();
-let feature = document.getElementById('features').value.trim();
+let email = document.getElementById('user-email').value.trim();
+let phone = parseInt(document.getElementById('user-phone').value.trim());
+let type = document.getElementById('pet-type').value.trim();
+let color = document.getElementById('pet-color').value.trim();
+let description = document.getElementById('pet-description').value.trim();
 
 //Create object to store variable values
 let lostPet = {
-    contactName: name.value,
-    contactEmail: email.value,
-    contactPhone: phone.value,
-    petSpecies: species.value,
-    petBreed: breed.value,
-    petColor: color.value,
-    feature: feature.value
+    Name: name.value,
+    userEmail: user - email.value,
+    userPhone: user - phone.value,
+    petType: pet - type.value,
+    petColor: pet - color.value,
+    petDescription: pet - description.value
 }
 
 //Create on-click event listener for submit button
-document.getElementById('contact-submit').addEventListener('click', function (event) {
+document.getElementById('report-lost-form"').addEventListener('click', function (event) {
     event.preventDefault();
     //form validation
     let valid = true;
@@ -27,27 +25,20 @@ document.getElementById('contact-submit').addEventListener('click', function (ev
         email === '' ||
         phone === '' ||
         (isNaN(phone == true))
-        || species === '' ||
-        breed === '' ||
+        || type === '' ||
         color === '' ||
-        features === '') {
+        description === '') {
         valid = false;
     }
     else {
         if (valid == true) {
-            console.log(name);
-            console.log(email);
-            console.log(phone);
-            console.log(species);
-            console.log(breed);
-            console.log(color);
-            console.log(feature);
+            console.log(sucess);
+            console.log('------------------------');
         }
 
         else {
             alert('Error: all fields are required');
         }
-
     }
 
     postRequest('/api/lost', lostPet)
