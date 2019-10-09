723ecca0618b221875217285fc55fab3baf3dd16
Jembae <jgilbertworks@gmail.com>
2019-10-08 19:21:43 -0700

    updated image url/javascript POST

--- app/javascript/reportLostLogic.js
+++ app/javascript/reportLostLogic.js
@@ -2,77 +2,53 @@
 let name = document.getElementById('name').value.trim();
 let email = document.getElementById('user-email').value.trim();
 let phone = parseInt(document.getElementById('user-phone').value.trim());
-let photo = document.getElementById("image-upload").files[0];
+let photo = document.getElementById('photo').value.trim();
+let petName = document.getElementById('pet-name').value.trim();
 let type = document.getElementById('pet-type').value.trim();
 let color = document.getElementById('pet-color').value.trim();
 let description = document.getElementById('pet-description').value.trim();
-let formData = new FormData();
-
-// !photo upload - review this section
-
-formData.append("photo", photo);
-fetch('/api/lost', {method: "POST", body: formData});// fix the fetch upload url
-
-async function SavePhoto(inp) 
-
-{
-    let user = { name:'john', age:34 };
-    let formData = new FormData();
-    let photo = inp.files[0];      
-         
-    formData.append("photo", photo);
-    formData.append("user", JSON.stringify(user));  
-    
-    try {
-       let r = await fetch('/api/lost', {method: "POST", body: formData}); 
-       console.log('HTTP response code:',r.status); 
-    } catch(e) {
-       console.log('Huston we have problem...:', e);
-    }
-    
-}
-// end photo upload
-
-//Create object to store variable values
-let lostPet = {
-    Name: name.value,
-    userEmail: email.value,
-    userPhone: phone.value,
-    petType: type.value,
-    petColor: color.value,
-    petDescription: description.value
-}
 
 //Create on-click event listener for submit button
 document.getElementById('report-lost-form').addEventListener('click', function (event) {
     event.preventDefault();
-    //form validation
+
+    let lostPet = {
+        userName: document.getElementById('name').value.trim(),
+        userEmail: document.getElementById('user-email').value.trim(),
+        userPhone: parseInt(document.getElementById('user-phone').value.trim()),
+        petPhoto: document.getElementById('photo').value.trim(),
+        petName: document.getElementById('pet-name').value.trim(),
+        petType: document.getElementById('pet-type').value.trim(),
+        petColor: document.getElementById('pet-color').value.trim(),
+        petDescription: document.getElementById('pet-description').value.trim()
+    }
+   
+    // form logic 
     let valid = true;
 
-    if (name === '' ||
-        email === '' ||
-        phone === '' ||
-        (isNaN(phone == true))
-        || type === '' ||
-        color === '' ||
-        description === '') {
+    if (document.getElementById('name').value === '' ||
+        document.getElementById('user-email').value === '' ||
+        document.getElementById('user-phone').value === '' ||
+        document.getElementById('photo').value === '' ||
+        document.getElementById('pet-name').value === '' ||
+        document.getElementById('pet-type').value === '' ||
+        document.getElementById('pet-color').value === '' ||
+        document.getElementById('pet-description').value === ''
+    ){
         valid = false;
+        alert('All fields must contain a values');
     }
-    else {
-        if (valid == true) {
-            console.log(sucess);
-            console.log('------------------------');
-        }
-
-        else {
-            alert('Error: all fields are required');
-        }
+    else
+    {
+        console.log(valid);
+        console.log(lostPet);
+        // alert('Thank you for your post.Please check your email for frequent updates');
     }
 
     postRequest('/api/lost', lostPet)
         .then(function () {
             document.getElementById('response').innerHTML = 'Thank you for your post.Please check your email for frequent updates';
-
+            console.log(data);
         });
 
     function postRequest(url, data) {
