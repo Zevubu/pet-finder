1decadfa4ee12205c6ce6f02d211b5ce4519b3ed
Zev Hoffman <zevubu@gmail.com>
2019-10-09 22:24:37 -0700

    Successfully pull from mysql and aplied it to leaflets on pins! for both lost and found. still need to figure out how to difereanciate them INSERT INTO lost (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_color, pet_description) used fetch

--- app/config/connection.js
+++ app/config/connection.js
@@ -4,7 +4,7 @@ let connection = mysql.createConnection({
     host:"localhost",
     user:"root",
     port: 3306,
-    password: "PASSWORDHERE",
+    password: "Anamel!keth!s",
     database: "petFinder_db"
 })
 
