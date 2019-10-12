8f1682cc7ae64154d3a57c5501ef202128a9a951
Zev Hoffman <zevubu@gmail.com>
2019-10-12 15:36:55 -0700

    updated seed

--- app/data/seeds.sql
+++ app/data/seeds.sql
@@ -1,17 +1,17 @@
-INSERT INTO lost (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description)
-VALUES ("Tom", "email@email.com", "555-555-5555","830 isabella st","Oakland","CA","94607","https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60","Puddles", "dog", "black","muddy");
+INSERT INTO lost (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description, pet_latlng)
+VALUES ("Tom", "email@email.com", "555-555-5555","6397 Telegraph Ave", "Oakland", "CA", "94609","https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60","Puddles", "dog", "black","muddy",'{"lat":37.849970,"lng":-122.260450}');
 
-INSERT INTO lost (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description)
-VALUES ("Tom", "email@email.com", "555-555-5555","304 valencia st","San francisco","CA","94103","https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60","Puddles","dog", "black","muddy");
+INSERT INTO lost (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description, pet_latlng)
+VALUES ("Tom", "email@email.com", "555-555-5555","304 valencia st","San francisco","CA","94103","https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60","Puddles","dog", "black","muddy",'{"lat":37.767940,"lng":-122.422250}');
 
-INSERT INTO lost (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description)
-VALUES ("Tom", "email@email.com", "555-555-5555","1995 University ave","berkeley","CA","94707","https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60","Puddles", "dog", "black","muddy");
+INSERT INTO lost (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description, pet_latlng)
+VALUES ("Tom", "email@email.com", "555-555-5555","1995 University ave","berkeley","CA","94707","https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=416&h=312&q=60","Puddles", "dog", "black","muddy",'{"lat":37.872230,"lng":-122.271290}');
 
-INSERT INTO found (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description)
-VALUES ("Bob", "email@email.com", "555-555-5555","920 Heinz Ave","Berkeley","CA","94710","https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80","Albery", "cat", "black","muddy");
+INSERT INTO found (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description, pet_latlng)
+VALUES ("Bob", "email@email.com", "555-555-5555","920 Heinz Ave","Berkeley","CA","94710","https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80","Albery", "cat", "black","muddy",'{"lat":37.853330,"lng":-122.290130}');
 
-INSERT INTO found (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description)
-VALUES ("Bob", "email@email.com", "555-555-5555","600 Guerrero st","San francisco","CA","94110","https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80","Albery", "cat", "black","muddy");
+INSERT INTO found (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description, pet_latlng)
+VALUES ("Bob", "email@email.com", "555-555-5555","600 Guerrero st","San francisco","CA","94110","https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80","Albery", "cat", "black","muddy",'{"lat":37.761560,"lng":-122.423900}');
 
-INSERT INTO found (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description)
-VALUES ("Bob", "email@email.com", "555-555-5555","519 18th st","Oakland","CA","94612","https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80","Albery", "cat", "black","muddy");
+INSERT INTO found (user_name, user_email, user_phone, user_address, user_city, user_state, user_zip, pet_photo, pet_name, pet_type, pet_color, pet_description, pet_latlng)
+VALUES ("Bob", "email@email.com", "555-555-5555","519 18th st","Oakland","CA","94612","https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80","Albery", "cat", "black","muddy",'{"lat":37.807678,"lng":-122.270866}');
