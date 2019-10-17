2484bbccabb77cd2fbb736b74979925118984915
Fred Saba <fred@fred-saba.com>
2019-10-16 20:34:32 -0700

    heroku deploy

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,12 +1,34 @@
-let mysql = require("mysql");
+require("dotenv").config();
+//Require mysql npm package to create a connection to the mysql database.
+var mysql = require("mysql");
+let keys = require('../../keys.js')
 
-let connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    port: 3306,
-    password: "PASSWORDHERE",
-    database: "petFinder_db"
-})
+let pw = keys.password.dbpw
+
+
+
+if (process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }
+  
+  else {
+    //else use localhost database for local development.
+    //MySQL password is passed into connection.js from the .env file using the dotenv npm package.
+  
+    var connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: pw,
+      database: "petFinder_db"
+    });
+    
+  }
+
+
+
+
 
 connection.connect(function(err){
     if(err)throw err;
